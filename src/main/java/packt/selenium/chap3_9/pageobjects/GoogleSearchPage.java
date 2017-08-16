1e1b940d491559145194c52ca0745e869de20e55
windsider <vrzherdev@gmail.com>
2017-08-16 19:28:13 +0300

    hello

--- src/main/java/packt/selenium/chap3_9/pageobjects/GoogleSearchPage.java
+++ src/main/java/packt/selenium/chap3_9/pageobjects/GoogleSearchPage.java
@@ -11,7 +11,7 @@ public class GoogleSearchPage {
     public GoogleSearchPage(WebDriver driver){
         this.driver = driver;
         System.out.println(driver.getTitle());
-        if (!driver.getTitle().equals("Mastering Selenium Testing Tools - Поиск  Google")){
+        if (!driver.getTitle().equals("Mastering Selenium Testing Tools - Поиск в Google")){
             throw new WrongPageException("Incorrect page for Google Search page");
         }
     }
